792a1d1b80f0ab09a020dfb3f2437bbd6cf153e9
K1R1LL117 <n17k17@yandex.ru>
2025-07-02 14:14:44 +0300

    Add JWT authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "api.v1.users.apps.UsersConfig",
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
